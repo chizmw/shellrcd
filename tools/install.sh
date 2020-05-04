ca0efc46a35e47f965261cc9fba8397a0234de10
Chisel <chisel@chizography.net>
2020-05-04 08:09:54 +0100

    Update tools/install.sh to pass shellcheck

--- tools/install.sh
+++ tools/install.sh
@@ -11,7 +11,7 @@
 #   $EDITOR ~/install-shellrcd.sh
 #   sh -c ~/install-shellrcd.sh
 
-set -euo pipefail
+set -eu
 
 # stolen from https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 command_exists() {
@@ -25,35 +25,31 @@ setup_color() {
         RED=$(printf '\033[31m')
         GREEN=$(printf '\033[32m')
         YELLOW=$(printf '\033[33m')
-        BLUE=$(printf '\033[34m')
-        BOLD=$(printf '\033[1m')
         RESET=$(printf '\033[m')
     else
         RED=""
         GREEN=""
         YELLOW=""
-        BLUE=""
-        BOLD=""
         RESET=""
     fi
 }
 
 already_installed() {
     # https://unix.stackexchange.com/questions/330660/prevent-grep-from-exiting-in-case-of-nomatch/403707#comment581721_330662
-    local count=$(grep -c '^## added by shellrcd ##' ${1:?Missing Argument} || :)
-    [[ $count -gt 0 ]]
+    count=$(grep -c '^## added by shellrcd ##' "${1:?Missing Argument}" || :)
+    [ "$count" -gt 0 ]
 }
 
 write_new_file() {
     rcfile=${1:?Missing rcfile}
     shell=${2:?Missing shell}
 
-    echo "#!${shell}" > ${rcfile}
-    append_block ${rcfile}
+    echo "#!${shell}" > "${rcfile}"
+    append_block "${rcfile}"
 }
 
 append_block() {
-    cat >> ${1:?Missing rcfile argument} <<EOF
+    cat >> "${1:?Missing rcfile argument}" <<EOF
 
 ## added by shellrcd ##
 if [ -f ~/.shellrc.d/source-relevant-files -a -x ~/.shellrc.d/source-relevant-files ]; then
@@ -68,46 +64,49 @@ append_or_create() {
     rcfile=${1:?Missing rcfile}
     shell=${2:?Missing shell}
 
-    if [ -f ${rcfile} ] || [ -h ${rcfile} ]; then
-        if already_installed ${rcfile}; then
+    if [ -f "${rcfile}" ] || [ -h "${rcfile}" ]; then
+        if already_installed "${rcfile}"; then
             echo "[${shell}] ${RED}shellrcd block already added to ${rcfile}${RESET}. Nothing to do."
         else
             echo "[${shell}] Adding shellrcd block to ${YELLOW}${rcfile}${RESET}..."
-            append_block ${rcfile}
+            append_block "${rcfile}"
         fi
     else
         echo "[${shell}] Creating ${YELLOW}${rcfile}${RESET} and adding shellrcd block..."
-        write_new_file $rcfile $(which ${shell})
+        write_new_file "$rcfile" "$(which "${shell}")"
     fi
 }
 
 setup_zshrc() {
-    local rcfile=~/.zshrc
-    local shell=zsh
+    rcfile=~/.zshrc
+    shell=zsh
 
     echo "[${shell}] ${YELLOW}Looking for an existing zsh config...${RESET}"
 
-    append_or_create ${rcfile} ${shell}
+    append_or_create "${rcfile}" "${shell}"
 }
 
 setup_bashrc() {
-    local rcfile
-    local shell=bash
+    unset rcfile
+    shell=bash
 
     echo "[${shell}] ${YELLOW}Looking for an existing bash config...${RESET}"
 
     # we should use .bash_profile if we're being "proper" on a mac
     # but for the rest of the world, .bashrc seems to be acceptable
     # https://serverfault.com/a/376264
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        echo "[${shell}] ${RED}MacOS${RESET} detected. Using ${YELLOW}.bash_profile${RESET}."
-        rcfile=~/.bash_profile
-    else
-        echo "[${shell}] ${RED}Non-MacOS${RESET} detected. Using ${YELLOW}.bashrc${RESET}."
-        rcfile=~/.bashrc
-    fi
+    case "$(uname)" in
+        Darwin*)
+            echo "[${shell}] ${RED}MacOS${RESET} detected. Using ${YELLOW}.bash_profile${RESET}."
+            rcfile=~/.bash_profile
+            ;;
+        *)
+            echo "[${shell}] ${RED}Non-MacOS${RESET} detected. Using ${YELLOW}.bashrc${RESET}."
+            rcfile=~/.bashrc
+            ;;
+    esac
 
-    append_or_create ${rcfile} ${shell}
+    append_or_create "${rcfile}" "${shell}"
 }
 
 main() {
